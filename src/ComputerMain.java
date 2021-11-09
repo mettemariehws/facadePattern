e395e08b84da43e239928be972dd3038f43a560f
marku <markusharen@gmail.com>
2021-11-09 11:10:52 +0100

    Facade setup, and main done

--- src/ComputerMain.java
+++ src/ComputerMain.java
@@ -2,5 +2,8 @@ public class ComputerMain {
 
     public static void main(String[] args) {
 
+        ComputerFacade computerFacade = new ComputerFacade();
+        computerFacade.turnOn();
+        computerFacade.turnOff();
     }
 }
